f2f86f3200f46de40c22a123df824d70509cc2e6
KatiaAydin <kaydin26@chaminet.org>
2024-01-26 17:05:54 -0800

    Created Trap subsystem

---
+++ src/main/java/frc/robot/subsystems/Trap.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Trap extends SubsystemBase {
+  /** Creates a new Trap. */
+  // I have no idea what they are using
+
+  public Trap() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
