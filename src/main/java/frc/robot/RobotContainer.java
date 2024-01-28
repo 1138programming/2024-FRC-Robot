6a4d4e58b440be2dbcaa7b12706caf6de284ec5a
Patrick S <90010357+WillSeguin@users.noreply.github.com>
2024-01-27 16:06:24 -0800

    Base Switched Base code to have Vortexes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,14 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Base.DriveWithJoysticks;
+import frc.robot.subsystems.Base;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -15,14 +21,143 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
+
+ 
 public class RobotContainer {
+  //Controller Ports (check in Driver Station, IDs may be different for each computer)
+  private static final int KLogitechPort = 0;
+  private static final int KXboxPort = 1;  
+  private static final int KStreamDeckPort = 2;
+  private static final int KTestingStreamDeckPort = 3;
+  private static final int KTuningStreamDeckPort = 4;
+
+  //Deadzone
+  private static final double KDeadZone = 0.05;
+  
+  //Joystick Axis IDs 
+  private static final int KLeftXAxis = 0;
+  private static final int KLeftYAxis = 1;
+  private static final int KRightXAxis = 2;
+  private static final int KRightYAxis = 3;
+
+  //Joystick Axis IDs 
+  private static final int KXboxLeftYAxis = 1;
+  private static final int KXboxRightYAxis = 5;
+  private static final int KXboxLeftXAxis = 0;
+  private static final int KXboxRightXAxis = 4;
+
+  //Logitech Button Constants
+  public static final int KLogitechButtonX = 1;
+  public static final int KLogitechButtonA = 2;
+  public static final int KLogitechButtonB = 3;
+  public static final int KLogitechButtonY = 4;
+  public static final int KLogitechLeftBumper = 5;
+  public static final int KLogitechRightBumper = 6;
+  public static final int KLogitechLeftTrigger = 7;
+  public static final int KLogitechRightTrigger = 8;
+
+  //Xbox Button Constants
+  public static final int KXboxButtonA = 1;
+  public static final int KXboxButtonB = 2;
+  public static final int KXboxButtonX = 3;
+  public static final int KXboxButtonY = 4;
+  public static final int KXboxLeftBumper = 5;
+  public static final int KXboxRightBumper = 6;
+  public static final int KXboxSelectButton = 7;
+  public static final int KXboxStartButton = 8;
+  public static final int KXboxLeftTrigger = 2;
+  public static final int KXboxRightTrigger = 3;
+
+  //Game Controllers
+  public static Joystick logitech;
+  public static Joystick compStreamDeck;
+  public static Joystick testStreamDeck;
+  public static Joystick tuningStreamDeck;
+  public static XboxController xbox; 
+  //Controller Buttons/Triggers
+  public JoystickButton logitechBtnX, logitechBtnA, logitechBtnB, logitechBtnY, logitechBtnLB, logitechBtnRB, logitechBtnLT, logitechBtnRT; //Logitech Button
+
+  public JoystickButton xboxBtnA, xboxBtnB, xboxBtnX, xboxBtnY, xboxBtnLB, xboxBtnRB, xboxBtnStrt, xboxBtnSelect;
+
+  public Trigger xboxBtnRT, xboxBtnLT;
+  
+  public JoystickButton comp1, comp2, comp3, comp4, comp5, comp6, comp7, comp8, comp9, comp10, comp11, comp12, comp13, comp14;
+
+  // Top Left SD = 1, numbered from left to right
+  public JoystickButton streamDeck1, streamDeck2, streamDeck3, streamDeck4, streamDeck5, streamDeck6, streamDeck7, streamDeck8, streamDeck9, // Vjoy 2
+    streamDeck10, streamDeck11, streamDeck12, streamDeck13, streamDeck14, streamDeck15;
+   
   // Subsystems
-
+  private final Base base = new Base();
   // Commands
-
+  private final DriveWithJoysticks drivewithJoysticks = new DriveWithJoysticks(base);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    base.setDefaultCommand(drivewithJoysticks);
     // Configure the trigger bindings
+
+
+    logitech = new Joystick(KLogitechPort); //Logitech Dual Action
+    xbox = new XboxController(KXboxPort);   //Xbox 360 for Windows
+    compStreamDeck = new Joystick(KStreamDeckPort);   //Stream Deck + vjoy
+    testStreamDeck = new Joystick(KTestingStreamDeckPort);   //Stream Deck + vjoy
+    tuningStreamDeck = new Joystick(KTuningStreamDeckPort);   //Stream Deck + vjoy
+
+    // Logitch Buttons 
+    logitechBtnX = new JoystickButton(logitech, KLogitechButtonX);
+    logitechBtnA = new JoystickButton(logitech, KLogitechButtonA);
+    logitechBtnB = new JoystickButton(logitech, KLogitechButtonB);
+    logitechBtnY = new JoystickButton(logitech, KLogitechButtonY);
+    logitechBtnLB = new JoystickButton(logitech, KLogitechLeftBumper);
+    logitechBtnRB = new JoystickButton(logitech, KLogitechRightBumper);
+    logitechBtnLT = new JoystickButton(logitech, KLogitechLeftTrigger);
+    logitechBtnRT = new JoystickButton(logitech, KLogitechRightTrigger);
+
+    // XBox Buttons
+    xboxBtnA = new JoystickButton(xbox, KXboxButtonA);
+  	xboxBtnB = new JoystickButton(xbox, KXboxButtonB);
+		xboxBtnX = new JoystickButton(xbox, KXboxButtonX);
+		xboxBtnY = new JoystickButton(xbox, KXboxButtonY);
+		xboxBtnLB = new JoystickButton(xbox, KXboxLeftBumper);
+    xboxBtnRB = new JoystickButton(xbox, KXboxRightBumper);
+    xboxBtnSelect = new JoystickButton(xbox, KXboxSelectButton);
+		xboxBtnStrt = new JoystickButton(xbox, KXboxStartButton);
+    xboxBtnLT = new Trigger(() -> (joystickThreshold(xbox.getRawAxis(KXboxLeftTrigger))));
+    xboxBtnRT = new Trigger(() -> (joystickThreshold(xbox.getRawAxis(KXboxRightTrigger))));
+
+    comp1 = new JoystickButton(compStreamDeck, 1);
+    comp2 = new JoystickButton(compStreamDeck, 2);
+    comp3 = new JoystickButton(compStreamDeck, 3);
+    comp4 = new JoystickButton(compStreamDeck, 4);
+    comp5 = new JoystickButton(compStreamDeck, 5);
+    comp6 = new JoystickButton(compStreamDeck, 6);
+    comp7 = new JoystickButton(compStreamDeck, 7);
+    comp8 = new JoystickButton(compStreamDeck, 8);
+    comp9 = new JoystickButton(compStreamDeck, 9);
+    comp10 = new JoystickButton(compStreamDeck, 10);
+    comp11 = new JoystickButton(compStreamDeck, 11);
+    comp12 = new JoystickButton(compStreamDeck, 12);
+    comp13 = new JoystickButton(compStreamDeck, 13);
+    comp14 = new JoystickButton(compStreamDeck, 14);
+
+    streamDeck1 = new JoystickButton(testStreamDeck, 1);
+    streamDeck2 = new JoystickButton(testStreamDeck, 2);
+    streamDeck3 = new JoystickButton(testStreamDeck, 3);
+    streamDeck4 = new JoystickButton(testStreamDeck, 4);
+    streamDeck5 = new JoystickButton(testStreamDeck, 5);
+    streamDeck6 = new JoystickButton(testStreamDeck, 6);
+    streamDeck7 = new JoystickButton(testStreamDeck, 7);
+    streamDeck8 = new JoystickButton(testStreamDeck, 8);
+    streamDeck9 = new JoystickButton(testStreamDeck, 9);
+    streamDeck10 = new JoystickButton(testStreamDeck, 10);
+    streamDeck11 = new JoystickButton(testStreamDeck, 11);
+    streamDeck12 = new JoystickButton(testStreamDeck, 12);
+    streamDeck13 = new JoystickButton(testStreamDeck, 13);
+    streamDeck14 = new JoystickButton(testStreamDeck, 14);
+    streamDeck15 = new JoystickButton(testStreamDeck, 15);
+  	
+    // Configure the button bindings
+    
     configureBindings();
   }
 
@@ -45,4 +180,89 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     return null;
   }
+  public double getLogiRightYAxis() {
+    final double Y = logitech.getRawAxis(KRightYAxis);
+    SmartDashboard.putNumber("getLogiRightYAxis", -Y);
+    if (Y > KDeadZone || Y < -KDeadZone)
+      return -Y;
+    else
+      return 0;
+  }
+  
+
+  public double getLogiLeftYAxis() {
+    final double Y = logitech.getY();
+    SmartDashboard.putNumber("getLogiLeftYAxis", -Y);
+    if(Y > KDeadZone || Y < -KDeadZone)
+      return -Y;
+    else 
+      return 0; 
+  }
+
+  public double getLogiRightXAxis() {
+    double X = logitech.getZ();
+    SmartDashboard.putNumber("getLogiRightXAxis", -X);
+    if (X > KDeadZone || X < -KDeadZone) {
+      return -X;
+    } else {
+      return 0; 
+    }
+  }
+
+  public double getLogiLeftXAxis() {
+    double X = logitech.getX();
+    SmartDashboard.putNumber("getLogiLeftXAxis", -X);
+    if (X > KDeadZone || X < -KDeadZone) {
+      return -X;
+    } else {
+      return 0;
+    }
+  }
+
+  public double getXboxLeftAxis() {
+    final double Y = xbox.getRawAxis(KXboxLeftYAxis);
+    if(Y > KDeadZone || Y < -KDeadZone)
+      return -Y;
+    else 
+      return 0;
+  }
+
+  public double getXboxLeftXAxis() {
+    final double X = xbox.getRawAxis(KXboxLeftXAxis);
+    if(X > KDeadZone || X < -KDeadZone)
+      return X;
+    else 
+      return 0;
+  }
+
+  public double getXboxRightXAxis() {
+    final double X = xbox.getRawAxis(KXboxRightXAxis);
+    if (X > KDeadZone || X < -KDeadZone)
+      return -X;
+    else
+      return 0;
+  }
+
+  public double getXboxLeftYAxis() {
+    final double Y = xbox.getRawAxis(KXboxLeftYAxis);
+    if(Y > KDeadZone || Y < -KDeadZone)
+      return -Y;
+    else 
+      return 0;
+  }
+
+  public double getXboxRightYAxis() {
+    final double Y = xbox.getRawAxis(KXboxRightYAxis);
+    if (Y > KDeadZone || Y < -KDeadZone)
+      return -Y;
+    else
+      return 0;
+  }
+public boolean joystickThreshold(double triggerValue) {
+    if (Math.abs(triggerValue) < .09) 
+      return false;
+    else 
+      return true;
+  }
 }
+
---
+++ src/main/java/frc/robot/subsystems/Base.java
@@ -0,0 +1,219 @@
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.SwerveDriveConstants.*;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Base extends SubsystemBase {
+  private SwerveModule frontLeftModule;
+  private SwerveModule frontRightModule;
+  private SwerveModule backLeftModule;
+  private SwerveModule backRightModule;
+
+  private AHRS gyro;
+
+  private SwerveDriveKinematics kinematics;
+  private SwerveDriveOdometry odometry;
+  private Pose2d pose;
+  
+  private double driveSpeedFactor;
+  private double rotSpeedFactor;
+
+  private boolean defenseMode = false;
+  
+  public Base() {
+    frontLeftModule = new SwerveModule(
+      KFrontLeftAngleMotorID,
+      KFrontLeftDriveMotorID,
+      KFrontLeftMagEncoderID,
+      KFrontLeftOffset,
+      KFrontLeftDriveReversed,
+      KFrontLeftAngleReversed
+    );
+    frontRightModule = new SwerveModule(
+      KFrontRightAngleMotorID,
+      KFrontRightDriveMotorID,
+      KFrontRightMagEncoderID, 
+      KFrontRightOffset,
+      KFrontRightDriveReversed,
+      KFrontRightAngleReversed
+    );
+    backLeftModule = new SwerveModule(
+      KBackLeftAngleMotorID,
+      KBackLeftDriveMotorID,
+      KBackLeftMagEncoderID, 
+      KBackLeftOffset,
+      KBackLeftDriveReversed,
+      KBackLeftAngleReversed
+    );
+    backRightModule = new SwerveModule(
+      KBackRightAngleMotorID,
+      KBackRightDriveMotorID,
+      KBackRightMagEncoderID, 
+      KBackRightOffset,
+      KBackRightDriveReversed,
+      KBackRightAngleReversed
+    );
+
+    gyro = new AHRS(SPI.Port.kMXP);
+    gyro.reset();
+
+    kinematics = new SwerveDriveKinematics(
+      KFrontLeftLocation, KFrontRightLocation,
+      KBackLeftLocation, KBackRightLocation
+    );
+    odometry = new SwerveDriveOdometry(kinematics, getHeading(), getPositions());
+    
+    driveSpeedFactor = KBaseDriveMidPercent;
+    rotSpeedFactor = KBaseRotMidPercent;
+
+    SmartDashboard.putNumber("X and Y PID", 0);
+    SmartDashboard.putNumber("rot P", 0);
+  }
+
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative, double maxDriveSpeedMPS) {
+    xSpeed *= maxDriveSpeedMPS;
+    ySpeed *= maxDriveSpeedMPS;
+    rot *= KMaxAngularSpeed * getRotSpeedFactor();
+    
+    //feeding parameter speeds into toSwerveModuleStates to get an array of SwerveModuleState objects
+    SwerveModuleState[] states =
+      kinematics.toSwerveModuleStates(
+        fieldRelative
+          ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, getHeading())
+          : new ChassisSpeeds(xSpeed, ySpeed, rot));
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, KPhysicalMaxDriveSpeedMPS);
+
+    if (defenseMode) {
+      lockWheels();
+    }
+    else {
+      //setting module states, aka moving the motors
+      frontLeftModule.setDesiredState(states[0]);
+      frontRightModule.setDesiredState(states[1]);
+      backLeftModule.setDesiredState(states[2]);
+      backRightModule.setDesiredState(states[3]);
+    }
+  }
+
+  public void lockWheels() {
+    frontLeftModule.lockWheel(); 
+    frontRightModule.lockWheel();
+    backLeftModule.lockWheel();
+    backRightModule.lockWheel();
+  }
+  
+  public void resetOdometry(Pose2d pose) {
+    resetAllRelEncoders();
+    this.pose = pose;
+    
+    odometry.resetPosition(getHeading(), getPositions(), pose);
+  }
+
+  public Pose2d getPose() {
+    return pose;
+  }
+
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    frontLeftModule.setDesiredState(desiredStates[0]);
+    frontRightModule.setDesiredState(desiredStates[1]);
+    backLeftModule.setDesiredState(desiredStates[2]);
+    backRightModule.setDesiredState(desiredStates[3]);
+  }
+
+  // recalibrates gyro offset
+  public void resetGyro() {
+    gyro.reset(); 
+    gyro.setAngleAdjustment(0);
+  }
+
+  public void resetGyro(double gyroOffset) {
+    gyro.reset();
+    gyro.setAngleAdjustment(gyroOffset);
+  }
+
+  public void resetAllRelEncoders() {
+    frontLeftModule.resetRelEncoders();
+    frontRightModule.resetRelEncoders();
+    backLeftModule.resetRelEncoders();
+    backRightModule.resetRelEncoders();
+  }
+
+  public SwerveModulePosition[] getPositions() {
+    SwerveModulePosition[] positions = new SwerveModulePosition[4];
+
+    positions[0] = frontLeftModule.getPosition();
+    positions[1] = frontRightModule.getPosition();
+    positions[2] = backLeftModule.getPosition();
+    positions[3] = backRightModule.getPosition();
+
+    return positions;
+  }
+  
+  public void resetOdometry() {
+    resetAllRelEncoders();
+    pose = new Pose2d();
+    
+    odometry.resetPosition(getHeading(), getPositions(), pose);
+  }
+
+  public Rotation2d getHeading() {
+    return Rotation2d.fromDegrees(getHeadingDeg());
+  }
+  public double getHeadingDeg() {
+    return -gyro.getAngle();
+  }
+
+  public double getRoll() {
+    return gyro.getRoll();
+  }
+  public double getPitch() {
+    return gyro.getPitch();
+  }
+  
+  public double getDriveSpeedFactor() {
+    return driveSpeedFactor;
+  }
+  public void setDriveSpeedFactor(double speedFactor) {
+    driveSpeedFactor = speedFactor;
+  }
+  
+  public double getRotSpeedFactor() {
+    return rotSpeedFactor;
+  }
+  public void setRotSpeedFactor(double speedFactor) {
+    rotSpeedFactor = speedFactor;
+  }
+  
+  public boolean getDefenseMode() {
+    return defenseMode;
+  }
+  public void setDefenseMode(boolean defenseMode) {
+    this.defenseMode = defenseMode;
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Gyro", getHeadingDeg());
+    SmartDashboard.putString("odometry pose", odometry.getPoseMeters().toString());
+    SmartDashboard.putNumber("BackLeftCanCoderPos", backLeftModule.getMagDegRaw());
+    SmartDashboard.putNumber("FrontLeftCanCoderPos", frontLeftModule.getMagDegRaw());
+    SmartDashboard.putNumber("BackRightCanCoderPos", backRightModule.getMagDegRaw());
+    SmartDashboard.putNumber("FrontRightCanCoderPos", frontRightModule.getMagDegRaw());
+    odometry.update(getHeading(), getPositions());
+    pose = odometry.getPoseMeters();
+  }
+}
+  
+  
