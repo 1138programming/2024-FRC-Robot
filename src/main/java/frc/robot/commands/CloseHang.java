4daf6ea1f58e243b48d7d9d5660b7ef64bdec9e5
KatiaAydin <kaydin26@chaminet.org>
2024-01-30 19:47:59 -0800

    tried to fix issue

--- src/main/java/frc/robot/commands/CloseHang.java
+++ src/main/java/frc/robot/commands/CloseHang.java
@@ -5,9 +5,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-
-import frc.robot.subsystems.Hang;
 import frc.robot.Constants;
+import frc.robot.subsystems.*;
 
 public class CloseHang extends Command {
   /** Creates a new CloseHang. */
