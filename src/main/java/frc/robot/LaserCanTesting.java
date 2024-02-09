6355c5f39f809d53dc660b648cc42cf58195eb21
Katie Tracton <119464101+A2Bocanegra@users.noreply.github.com>
2024-02-08 18:05:15 -0800

    test code

--- src/main/java/frc/robot/LaserCanTesting.java
+++ src/main/java/frc/robot/LaserCanTesting.java
@@ -4,21 +4,16 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import au.grapplerobotics.LaserCan;
 import au.grapplerobotics.ConfigurationFailedException;
-import edu.wpi.first.wpilibj.TimedRobot;
 
 public class LaserCanTesting extends SubsystemBase {
   /** Creates a new LaserCanTesting. */
   private LaserCan laserCan;
   public LaserCanTesting() {
     laserCan = new LaserCan(0);
-  }
-
-
-  @Override
-  public void init() {
     try {
     laserCan.setRangingMode(LaserCan.RangingMode.SHORT);
     laserCan.setRegionOfInterest(new LaserCan.RegionOfInterest(6, 6, 16, 16));
@@ -26,17 +21,26 @@ public class LaserCanTesting extends SubsystemBase {
     } catch (ConfigurationFailedException e){
       System.out.println("Configuration failed! " + e);
     }
-    // This method will be called once per scheduler run
   }
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("LaserCanDistance", laserCanDistance());
+    SmartDashboard.getString("LaserCanDisStatus", laserCanDisStatus());
+  }
+
+  public double laserCanDistance(){
+    LaserCan.Measurement measurement = laserCan.getMeasurement();
+    double distance = measurement.distance_mm;
+    return distance;
+  }
+
+  public String laserCanDisStatus(){
     LaserCan.Measurement measurement = laserCan.getMeasurement();
     if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
-      System.out.println("The target is " + measurement.distance_mm + "mm away!");
+      return "The target is " + measurement.distance_mm + "mm away!";
     } else {
-      System.out.println("Oh no! The target is out of range, or we can't get a reliable measurement!");
-      // You can still use distance_mm in here, if you're ok tolerating a clamped value or an unreliable measurement.
+      return "Oh no! The target is out of range, or we can't get a reliable measurement!";
     }
   }
 
