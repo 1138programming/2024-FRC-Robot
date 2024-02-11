9cbb454e26c9d23535e386a08bdacb58fbf8dbaf
Katia Aydin <142551967+Katia261928@users.noreply.github.com>
2024-02-10 20:14:52 -0800

    changed motor controller and added laser

--- src/main/java/frc/robot/Subsystems/Hang.java
+++ src/main/java/frc/robot/Subsystems/Hang.java
@@ -7,39 +7,54 @@ package frc.robot.Subsystems;
 import static frc.robot.Constants.Hang.*;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+import au.grapplerobotics.ConfigurationFailedException;
+import au.grapplerobotics.LaserCan;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 // Neo Motor sparkmax
 // 2 piston pneumatics double solenoid
 // an built in encoder and 2 limit switches
 // pneumatics are entirely seperate but have the same function
 // make sure to add speed constants - don't just name it speed -> scope issues
+// might have to add laser
 
 public class Hang extends SubsystemBase {
   /** Creates a new Hang. */
-  private CANSparkMax hangMotor;
+  private CANSparkFlex hangMotor;
   private DigitalInput hangLimitSwitchUp;
-  private DigitalInput hangLimitSwitchBottom;
+  //private DigitalInput hangLimitSwitchBottom;
 
   private DoubleSolenoid hangPistonLeft;
   private DoubleSolenoid hangPistonRight;
+  private LaserCan hangLaserCanBottom;
 
   public Hang() {
-    hangMotor = new CANSparkMax(KHangMotorID, MotorType.kBrushless);
+    hangMotor = new CANSparkFlex(KHangMotorID, MotorType.kBrushless);
     hangLimitSwitchUp = new DigitalInput(KHangLimitSwitchUp);
-    hangLimitSwitchBottom = new DigitalInput(KHangLimitSwitchDown);
-    // create constants for the pneumatics
-    //figure out motor type
-    // which one REVPH and CTREPCM
+    //hangLimitSwitchBottom = new DigitalInput(KHangLimitSwitchDown);
     hangPistonLeft = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, KHangPistonLeftInID, KHangPistonLeftOutID);
     hangPistonLeft.set(DoubleSolenoid.Value.kReverse);
     hangPistonRight = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, KHangPistonRightInID, KHangPistonRightOutID);
     hangPistonRight.set(DoubleSolenoid.Value.kReverse);
+
+    hangLaserCanBottom = new LaserCan(5);
+    try {
+    hangLaserCanBottom.setRangingMode(LaserCan.RangingMode.SHORT);
+    hangLaserCanBottom.setRegionOfInterest(new LaserCan.RegionOfInterest(6, 6, 16, 16));
+    hangLaserCanBottom.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+    } catch (ConfigurationFailedException e){
+      System.out.println("Configuration failed! " + e);
+    }
   }
 
   @Override
@@ -49,6 +64,28 @@ public class Hang extends SubsystemBase {
 // add two methods on and off for each pneumatic system
 // make them to things in the commands later
 // add pneumatics into methods and commands
+public double laserCanDistanceMM(){
+  LaserCan.Measurement measurement = hangLaserCanBottom.getMeasurement();
+  double distance = measurement.distance_mm;
+  return distance;
+}
+
+public double laserCanDistanceInch(){
+  LaserCan.Measurement measurement = hangLaserCanBottom.getMeasurement();
+  double distance = measurement.distance_mm;
+  return distance/25.4;
+}
+
+public String laserCanDisStatus(){
+  LaserCan.Measurement measurement = hangLaserCanBottom.getMeasurement();
+  if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+    return "The target is " + measurement.distance_mm + "mm away!";
+  } else {
+    return "Oh no! The target is out of range, or we can't get a reliable measurement!";
+  }
+}
+
+
 
   public void setHangHookPosUp(double hangSpeed){
 
@@ -61,14 +98,20 @@ public class Hang extends SubsystemBase {
   
 
 // Moves hang to a set position
-
-  public void setHangHookPosDown(double hangSpeed){
+public void setHangHookPosDown(double hangSpeed){
+  if (laserCanDistanceInch() >= 2){
+    hangMotor.getEncoder().setPosition(KHangSetPositionDown);
+    hangMotor.set(KHangMotorSpeedDown);
+  }
+  hangMotor.set(0);
+}
+  /*public void setHangHookPosDown(double hangSpeed){
     if (hangMotor.getEncoder().getPosition() <= 0 && !getHangBottomLimitSwitch()){
       hangMotor.getEncoder().setPosition(KHangSetPositionDown);
       hangMotor.set(KHangMotorSpeedDown);
      }
     hangMotor.set(0);
-  }
+  }*/
   
   public double getHangPosition(){
     return hangMotor.getEncoder().getPosition();
@@ -78,13 +121,13 @@ public class Hang extends SubsystemBase {
     return hangLimitSwitchUp.get();
   }
 
-  public boolean getHangBottomLimitSwitch(){
-    return hangLimitSwitchBottom.get();
-  }
+ // public boolean getHangBottomLimitSwitch(){
+   // return hangLimitSwitchBottom.get();
+  //}
 // Only moves up and down to the limit switches
   public void moveHangMotor(double hangSpeed){
     if(hangSpeed <= 0 ){
-      if (getHangBottomLimitSwitch()){
+      if (laserCanDistanceInch() <= 2){
         hangMotor.set(0);
       }
     }
