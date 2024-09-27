c21f2e9a633b79494faaf2204f47450c92093537
Glutinfree <131933467+DriginWater@users.noreply.github.com>
2024-09-26 17:39:51 -0700

    Create DoSomethingMotor

--- src/main/java/frc/robot/subsystems/DoSomethingMotor.java
+++ src/main/java/frc/robot/subsystems/DoSomethingMotor.java
@@ -5,11 +5,28 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import static frc.robot.Constants.*;
 
 public class DoSomethingMotor extends SubsystemBase {
+  private CANSparkMax NicoMotor1;
   /** Creates a new DoSomethingMotor. */
-  public DoSomethingMotor() {}
+  public DoSomethingMotor() {
+    NicoMotor1 = new CANSparkMax(KNicoMotor1ID, MotorType.kBrushless);
 
+    NicoMotor1.setIdleMode(IdleMode.kCoast)
+    NicoMotor1.setInverted((KNicoMotor1IDInverted));;
+  } 
+
+  public void spinNicoMotor1(double speed) {
+    NicoMotor1.set(speed);
+  }
+
+  public void stopNicoMotor1() {
+    NicoMotor1.set(speed:0);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
